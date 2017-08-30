a6c81ddb3a027996215ef6531d5f4085fd4db619
Justin Zimmer <jtzimmer6@gmail.com>
2017-08-30 16:31:08 -0700

    Initial commit

---
+++ compilebook.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+outputfile=$1
+CHAPTERS=$(for chapter in ../Manuscript/*.md ; do echo $chapter; done) 
+
+if [ "$outputfile" == "" ]
+then
+  echo "You must provide a filename with appropriate extension (i.e. file.epub for ePub) for this to work!"
+  exit
+fi
+pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc --toc-depth=2 -o Output/$outputfile
+#cat ../Manuscript/* | pandoc -s -o Output/$outputfile --toc
