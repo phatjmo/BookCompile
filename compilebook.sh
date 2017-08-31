a9f9544d38e417b9668ad2924dcbf8542a58078b
Justin Zimmer <jtzimmer6@gmail.com>
2017-08-30 19:44:59 -0700

    more fixes

--- compilebook.sh
+++ compilebook.sh
@@ -7,6 +7,6 @@ then
   echo "You must provide a filename with appropriate extension (i.e. file.epub for ePub) for this to work!"
   exit
 fi
-pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc --toc-depth=2 -o Output/$outputfile
+pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc -o Output/$outputfile
 #cat ../Manuscript/* | pandoc -s -o Output/$outputfile --toc
 open Output/$outputfile
