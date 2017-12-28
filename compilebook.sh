b4549088b2b4a5c865693f81b302c500e6a755c5
Justin Zimmer <jtzimmer6@gmail.com>
2017-12-27 17:59:54 -0700

    adding literata css and fonts

--- compilebook.sh
+++ compilebook.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 outputfile=$1
 CHAPTERS=$(for chapter in ../Manuscript/*.md ; do echo $chapter; done) 
-
+FONT="./Styles/Fonts/Literata/literata*.woff"
 if [ "$outputfile" == "" ]
 then
   echo "You must provide a filename with appropriate extension (i.e. file.epub for ePub) for this to work!"
   exit
 fi
-pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc -o Output/$outputfile
+pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc --epub-embed-font=$FONT -o Output/$outputfile
 # To embed fonts...
 # pandoc $CHAPTERS ../Manuscript/metadata.yaml -s --toc -o Output/$outputfile --epub-embed-font="Styles/Fonts/Ubuntu/Ubuntu-*.ttf"
 #cat ../Manuscript/* | pandoc -s -o Output/$outputfile --toc
--- compilechaps.sh
+++ compilechaps.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 extension=$1 
-
+FONT="./Styles/Fonts/Literata/literata*.woff"
 if [ "$extension" == "" ]
 then
   echo "You didn't specify an extension, so I'm gonna assume you want PDFs!"
@@ -9,5 +9,5 @@ fi
 for chapter in ../Manuscript/*.md 
 do 
     trimchap=${chapter:14}
-    pandoc $chapter -s -o Output/${trimchap%.*}.${extension,,}
+    pandoc $chapter -s --epub-embed-font=$FONT -o Output/${trimchap%.*}.${extension,,}
 done
